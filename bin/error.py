396a7fa386b71c0a6405ae714fc8aa536b71e5fd
Stanislav Mosin <104845230+MosinPRSS@users.noreply.github.com>
2024-11-22 11:08:47 +0300

    lmao

--- bin/error.py
+++ bin/error.py
@@ -1,15 +1,37 @@
-import tkinter as Tk
+from tkinter import *
+from tkinter import ttk
 # import OsType as IDEN
 
 def ERROR(receive): 
-    window = Tk.Tk()
+    window = Tk()
     if receive == "UNKNOWN":
+        ERRORMESSAGE = "Error Code 00 \nChrome (Chromium) is not installed."
         # Geometry and Style
         window.title("Serious Error Has Occured!")
         window.geometry("400x100")
-        window.iconbitmap("applogo.ico")
         window.resizable(False, False)
+
+        # Widgets
+
+        # Text 
+        txtError = Text(window, height = 5, width = 52)
+        txtError = Label(window, text=ERRORMESSAGE)
+        # Buttons
+        btnShowLog = ttk.Button(text="Show Logs")
+        btnExit = ttk.Button(text="Exit")
+
+        # Placing of objects
+        txtError.place(anchor=N)
+        btnShowLog.place(anchor=SW)
+        btnExit.place(anchor=SE)
+
+        # Pack Widgets
+        txtError.pack()
+        btnExit.pack()
+        btnShowLog.pack()
+
+        # DONT CHANGE OR PASTE
         window.mainloop()
+
         
 
-ERROR("UNKNOWN")
